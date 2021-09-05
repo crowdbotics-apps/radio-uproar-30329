9cd408d5a56de26e54f0fd07c1dcd07a50796532
Crowdbotics <team@crowdbotics.com>
2021-09-05 13:56:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "radio_uproar_30329Identifier",
+  url: "https://radio_uproar_30329Identifier.botics.co",
+  api: "https://radio_uproar_30329Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
